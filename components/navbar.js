c6bdbd1fc7d48fc7c1de6efdbccf37f1d7cc0b9e
Ensoto24 <71364801+Ensoto24@users.noreply.github.com>
2022-06-14 22:48:39 -0500

    layout

---
+++ components/navbar.js
@@ -0,0 +1,15 @@
+const navbar = () => {
+    return (
+        <nav>
+           <div className="logo">
+              <h1>Ninfa's</h1>
+           </div>
+           <a>Menu</a>   
+           <a>About Us</a>   
+           <a>Contact</a>   
+           <a>Order Now</a>   
+        </nav>
+    );
+}
+
+export default navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+z
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
