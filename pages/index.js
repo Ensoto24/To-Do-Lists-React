c2835bf3014066583593774bd9aec8f0356de45d
Umair Ahmed <umairahmed.dev@gmail.com>
2022-06-17 02:32:21 -0500

    hours section

--- pages/index.js
+++ pages/index.js
@@ -8,24 +8,56 @@ import styles from "../styles/home.module.css";
 
 export default function Home() {
   return (
-    <><div className={styles.heroImage}>
-      <Image src={mexico} width={1440} height={733} />
-    </div>
-    <div className={styles.aboutMain}>
-      <div className={styles.aboutText}>
-        <p>Every Day we continue a <span className={styles.aboutUnderline}>wonderful tradition</span>
-          by serving you and your family with the <span className={styles.aboutUnderline}>same affection
-          and quality</span> that our founder, <span className={styles.aboutUnderline}>Maria Ninfa Rodriquez Laurenzo,</span>
-          established in 1973.
-        </p>
-        <p>
-          Known to everyone as “Mama”, <span className={styles.aboutUnderline}>her legacy began in a small
-          10-table restaurant on Navigation.</span> Using her own pots and
-          pans, <span className={styles.aboutUnderline}>she created this world-renowned tradition</span> of serving
-           great food and delicious Margaritas.
-        </p>
+    <>
+      <div className={styles.heroImage}>
+        <Image src={mexico} width={1440} height={733} />
       </div>
+      <div className={styles.aboutMain}>
+        <div className={styles.aboutText}>
+          <p>
+            Every Day we continue a{" "}
+            <span className={styles.aboutUnderline}>wonderful tradition</span>
+            by serving you and your family with the{" "}
+            <span className={styles.aboutUnderline}>
+              same affection and quality
+            </span>{" "}
+            that our founder,{" "}
+            <span className={styles.aboutUnderline}>
+              Maria Ninfa Rodriquez Laurenzo,
+            </span>
+            established in 1973.
+          </p>
+          <p>
+            Known to everyone as “Mama”,{" "}
+            <span className={styles.aboutUnderline}>
+              her legacy began in a small 10-table restaurant on Navigation.
+            </span>{" "}
+            Using her own pots and pans,{" "}
+            <span className={styles.aboutUnderline}>
+              she created this world-renowned tradition
+            </span>{" "}
+            of serving great food and delicious Margaritas.
+          </p>
+        </div>
       </div>
-      </>
+      <div className={styles.mainHours}>
+        <h2>Hours</h2>
+        <div>
+          <div className={styles.hours}>
+            <h2>Mon-Fri</h2>
+            <p>11:00AM - 10:00PM</p>
+          </div>
+          <div className={styles.hours}>
+            <h2>Sat</h2>
+            <p>9:00AM - 11:00PM</p>
+          </div>
+          <div className={styles.hours}>
+            <h2>Sun</h2>
+            <p>9:00AM - 9:00PM</p>
+          </div>
+        </div>
+        <button className={styles.order}>Order Now</button>
+      </div>
+    </>
   );
 }
