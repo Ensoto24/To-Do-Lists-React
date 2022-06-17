7b261272170354395c2460fba0d3c1336c019bc6
Ensoto24 <71364801+Ensoto24@users.noreply.github.com>
2022-06-17 02:31:09 -0500

    line

--- components/footer.js
+++ components/footer.js
@@ -1,19 +1,21 @@
 import styles from "../styles/footer.module.css";
 import Image from "next/image";
 
-
 const footer = () => {
-    return (
-        <div className={styles.footerMain}>
-           <div className={styles.footerContact}>
-                <h2>The Original Mexican Restaurant</h2>
-                <p>5730 Hwy 6<br/>Missouri City, TX 77459<br/>Tel: 281-499-5070</p>
-           </div>
-           <div className={styles.footerMap}>
-           </div>
-        </div>
-    );
-}
+  return (
+    <div className={styles.footerMain}>
+      <div className={styles.footerContact}>
+        <h2>The Original Mexican Restaurant</h2>
+        <p>
+          5730 Hwy 6<br />
+          Missouri City, TX 77459
+          <br />
+          Tel: 281-499-5070
+        </p>
+      </div>
+      <div className={styles.footerMap}></div>
+    </div>
+  );
+};
 
 export default footer;
-
--- pages/index.js
+++ pages/index.js
@@ -3,29 +3,47 @@ import footer from "../components/Footer";
 import Link from "next/link";
 import Navbar from "../components/Navbar";
 import Image from "next/image";
+import line from "../public/line.svg";
 import mexico from "../public/mexico.png";
 import styles from "../styles/home.module.css";
 
 export default function Home() {
   return (
-    <><div className={styles.heroImage}>
-      <Image src={mexico} width={1440} height={733} />
-    </div>
-    <div className={styles.aboutMain}>
-      <div className={styles.aboutText}>
-        <p>Every Day we continue a <span className={styles.aboutUnderline}>wonderful tradition</span>
-          by serving you and your family with the <span className={styles.aboutUnderline}>same affection
-          and quality</span> that our founder, <span className={styles.aboutUnderline}>Maria Ninfa Rodriquez Laurenzo,</span>
-          established in 1973.
-        </p>
-        <p>
-          Known to everyone as “Mama”, <span className={styles.aboutUnderline}>her legacy began in a small
-          10-table restaurant on Navigation.</span> Using her own pots and
-          pans, <span className={styles.aboutUnderline}>she created this world-renowned tradition</span> of serving
-           great food and delicious Margaritas.
-        </p>
+    <>
+      <div className={styles.heroImage}>
+        <Image src={mexico} width={1440} height={733} />
       </div>
+      <div className={styles.aboutMain}>
+        <div className={styles.aboutText}>
+          <p>
+            Every Day we continue a{" "}
+            <span className={styles.aboutUnderline}>wonderful tradition</span>
+            by serving you and your family with the{" "}
+            <span className={styles.aboutUnderline}>
+              same affection and quality
+            </span>{" "}
+            that our founder,{" "}
+            <span className={styles.aboutUnderline}>
+              Maria Ninfa Rodriquez Laurenzo,
+            </span>
+            established in 1973.
+          </p>
+          <div className={styles.line}>
+            <img src="../public/line.svg" alt="" />
+          </div>
+          <p>
+            Known to everyone as “Mama”,{" "}
+            <span className={styles.aboutUnderline}>
+              her legacy began in a small 10-table restaurant on Navigation.
+            </span>{" "}
+            Using her own pots and pans,{" "}
+            <span className={styles.aboutUnderline}>
+              she created this world-renowned tradition
+            </span>{" "}
+            of serving great food and delicious Margaritas.
+          </p>
+        </div>
       </div>
-      </>
+    </>
   );
 }
