abee3880e490ce2e7057812e572b6a158063ee89
Umair Ahmed <umairahmed.dev@gmail.com>
2022-06-20 23:03:41 -0500

    map section complete

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,6 @@
 import styles from "../styles/footer.module.css";
 import Image from "next/image";
+import ninfasMap from "../public/ninfas_map.png";
 
 const Footer = () => {
   return (
@@ -14,7 +15,19 @@ const Footer = () => {
         </p>
       </div>
       <div className={styles.footerMap}>
-        <h1></h1>
+        <a
+          href="https://www.google.com/maps/place/Ninfa's/@29.5702389,-95.5714415,17z/data=!3m1!4b1!4m5!3m4!1s0x8640e5fa5bce4469:0x5ef929721c8db434!8m2!3d29.5702407!4d-95.5692502"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          <Image
+            src={ninfasMap}
+            alt="mama ninfas map location"
+            width={600}
+            height={350}
+          ></Image>
+        </a>
       </div>
     </div>
   );
