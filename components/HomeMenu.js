ae1ecdd6ae46ec9ef6a505e15962792c29392d43
Ensoto24 <71364801+Ensoto24@users.noreply.github.com>
2022-06-24 00:23:43 -0500

    HomeMenu pics

--- components/HomeMenu.js
+++ components/HomeMenu.js
@@ -1,40 +1,72 @@
 import styles from "../styles/homeMenu.module.css";
 import Image from "next/image";
 import burritos from "../public/burritos.png";
+import queso from "../public/queso.png";
+import nachos from "../public/nachos.png";
+import onetaco from "../public/onetaco.png";
+import quesadillas from "../public/quesadillas.png";
+import tejascombo from "../public/tejascombo.png";
+import guac from "../public/guac.png";
 
 const homeMenu = () => {
   return (
     <div className={styles.homeMenuMain}>
       <div className={styles.homeMenuItem}>
         <div className={styles.homeMenuImage}>
-          <Image src={burritos} alt="Ninfa's" width={378.67} height={253.38} />
+          <Image src={queso} alt="Ninfa's" width={378.67} height={253.38} />
         </div>
         <div className={styles.homeMenuDesc}>
-          <p>La Zona Rosa</p>
+          <p>Chile Con Queso</p>
         </div>
       </div>
       <div className={styles.homeMenuItem}>
         <div className={styles.homeMenuImage}>
-          <Image src={burritos} alt="Ninfa's" width={378.67} height={253.38} />
+          <Image
+            src={quesadillas}
+            alt="Ninfa's"
+            width={378.67}
+            height={253.38}
+          />
         </div>
         <div className={styles.homeMenuDesc}>
-          <p>La Zona Rosa</p>
+          <p>Quesadillas</p>
         </div>
       </div>
       <div className={styles.homeMenuItem}>
         <div className={styles.homeMenuImage}>
-          <Image src={burritos} alt="Ninfa's" width={378.67} height={253.38} />
+          <Image
+            src={tejascombo}
+            alt="Ninfa's"
+            width={378.67}
+            height={253.38}
+          />
         </div>
         <div className={styles.homeMenuDesc}>
-          <p>La Zona Rosa</p>
+          <p>Tejas Combo</p>
         </div>
       </div>
       <div className={styles.homeMenuItem}>
         <div className={styles.homeMenuImage}>
-          <Image src={burritos} alt="Ninfa's" width={378.67} height={253.38} />
+          <Image src={onetaco} alt="Ninfa's" width={378.67} height={253.38} />
         </div>
         <div className={styles.homeMenuDesc}>
-          <p>La Zona Rosa</p>
+          <p>One Taco A La Ninfa</p>
+        </div>
+      </div>
+      <div className={styles.homeMenuItem}>
+        <div className={styles.homeMenuImage}>
+          <Image src={guac} alt="Ninfa's" width={378.67} height={253.38} />
+        </div>
+        <div className={styles.homeMenuDesc}>
+          <p>Guacamole</p>
+        </div>
+      </div>
+      <div className={styles.homeMenuItem}>
+        <div className={styles.homeMenuImage}>
+          <Image src={nachos} alt="Ninfa's" width={378.67} height={253.38} />
+        </div>
+        <div className={styles.homeMenuDesc}>
+          <p>Nachos</p>
         </div>
       </div>
     </div>
