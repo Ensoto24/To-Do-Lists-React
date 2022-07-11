1817cbb5c3c132c8f69e8bdf5fa561a1ec33d916
Austin Riddell <git@austinriddell.com>
2022-07-11 00:10:53 -0500

    add motion

--- components/About.js
+++ components/About.js
@@ -1,10 +1,15 @@
+import { motion } from "framer-motion";
 import styles from "../styles/about.module.css";
 
 export default function About() {
   return (
     <div id="about-us" className={styles.aboutMain}>
       <div className={styles.aboutText}>
-        <p>
+        <motion.p
+          initial={{ opacity: 0, y: 256 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ type: "spring", bounce: 0.01 }}
+        >
           Every Day we continue a{" "}
           <span className={styles.aboutUnderline}>wonderful tradition</span> by
           serving you and your family with the{" "}
@@ -16,13 +21,17 @@ export default function About() {
             Maria Ninfa Rodriquez Laurenzo,
           </span>
           established in 1973.
-        </p>
+        </motion.p>
       </div>
       <div className={styles.aboutLine}>
         <img src="line.svg" />
       </div>
       <div className={styles.aboutText}>
-        <p>
+        <motion.p
+          initial={{ opacity: 0, y: 256 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ type: "spring", bounce: 0.01 }}
+        >
           Known to everyone as “Mama”,{" "}
           <span className={styles.aboutUnderline}>
             her legacy began in a small 10-table restaurant on Navigation.
@@ -32,7 +41,7 @@ export default function About() {
             she created this world-renowned tradition
           </span>{" "}
           of serving great food and delicious Margaritas.
-        </p>
+        </motion.p>
       </div>
     </div>
   );
