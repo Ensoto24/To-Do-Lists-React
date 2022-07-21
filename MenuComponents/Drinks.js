4964f7f6ec1e1ee9512065ac78744cffe478a78a
Umair Ahmed <umairahmed.dev@gmail.com>
2022-07-21 17:44:12 -0500

    updated drink menu

--- MenuComponents/Drinks.js
+++ MenuComponents/Drinks.js
@@ -13,7 +13,7 @@ export default function Drinks() {
           <p>
             Don Julio 1942 Tequila, Grand Marnier 100 Yr., and fresh lime juice.
           </p>
-          <h3>$21.95</h3>
+          <h3>$24.00</h3>
         </div>
 
         <div className={styles.item}>
@@ -23,7 +23,7 @@ export default function Drinks() {
             Jose Cuervo Reserva De La Familia Anejo Tequila, Grand Marnier, and
             fresh lime juice.
           </p>
-          <h3>$17.95</h3>
+          <h3>$20.00</h3>
         </div>
 
         <div className={styles.item}>
@@ -40,7 +40,7 @@ export default function Drinks() {
             spiced finish. <br />
             Don Julio 1942 - Caramel and toffee flavor with a vanilla fragrance.
           </p>
-          <h3>$17.95</h3>
+          <h3>$20.00</h3>
         </div>
 
         <div className={styles.item}>
@@ -50,7 +50,7 @@ export default function Drinks() {
             Don Julio Reposado Tequila, Presidente Brandy, Cointreau, and fresh
             lime juice.
           </p>
-          <h3>$10.00</h3>
+          <h3>$11.00</h3>
         </div>
 
         <div className={styles.item}>
@@ -60,28 +60,28 @@ export default function Drinks() {
             Jose Cuervo Platino Reserva De La Familia Tequila, Grand Marnier,
             and fresh lime juice.
           </p>
-          <h3>$9.50</h3>
+          <h3>$10.50</h3>
         </div>
 
         <div className={styles.item}>
           <hr />
           <h2>Anejo Sensation Margarita</h2>
           <p>Herradura Anejo Tequila, Grand Marnier, and fresh lime juice.</p>
-          <h3>$10.00</h3>
+          <h3>$11.00</h3>
         </div>
 
         <div className={styles.item}>
           <hr />
           <h2>The Perfect Margarita</h2>
           <p>Patron Silver Tequila, Patron Citronge, and fresh lime juice.</p>
-          <h3>$9.50</h3>
+          <h3>$10.50</h3>
         </div>
 
         <div className={styles.item}>
           <hr />
           <h2>The Silver Cadillac Margarita</h2>
           <p>Don Julio Blanco Tequila, Grand Marnier, and fresh lime juice.</p>
-          <h3>$9.50</h3>
+          <h3>$10.50</h3>
         </div>
 
         <div className={styles.item}>
@@ -91,7 +91,7 @@ export default function Drinks() {
             Jose Cuervo Tradicional Tequila, Grand Marnier, and fresh lime
             juice.
           </p>
-          <h3>$9.00</h3>
+          <h3>$10.00</h3>
         </div>
 
         <div className={styles.item}>
@@ -101,7 +101,7 @@ export default function Drinks() {
             Jose Cuervo Black Medallion Tequila, Grand Marnier, and fresh lime
             juice.
           </p>
-          <h3>$9.00</h3>
+          <h3>$10.00</h3>
         </div>
 
         <div className={styles.item}>
@@ -111,7 +111,7 @@ export default function Drinks() {
             Sauza Gold Tequila and Grand Marnier with a splash of orange juice
             and fresh lime juice.
           </p>
-          <h3>$8.50</h3>
+          <h3>$9.50</h3>
         </div>
 
         <div className={styles.item}>
@@ -121,7 +121,7 @@ export default function Drinks() {
             Margaritaville Gold and Silver, triple sec, fresh lime juice, and a
             splash of Blue Curaçao.
           </p>
-          <h3>$7.95</h3>
+          <h3>$9.00</h3>
         </div>
 
         <div className={styles.item}>
@@ -130,14 +130,14 @@ export default function Drinks() {
           <p>
             Jose Cuervo Especial Tequila, Grand Marnier, and fresh lime juice.
           </p>
-          <h3>$7.50</h3>
+          <h3>$8.50</h3>
         </div>
 
         <div className={styles.item}>
           <hr />
           <h2>Mango or Strawberry Ninfarita</h2>
           <p>Our famous Ninfarita goes tropical.</p>
-          <h3>$7.00</h3>
+          <h3>$8.00</h3>
         </div>
 
         <div className={styles.item}>
@@ -145,8 +145,8 @@ export default function Drinks() {
           <h2>Ninfarita</h2>
           <p>Our famous Ninfarita goes tropical.</p>
           <h3>
-            Reg. $6.00 <br />
-            Pitcher. $22.95
+            Reg. $7.00 <br />
+            Pitcher. $24.00
           </h3>
         </div>
 
@@ -154,14 +154,14 @@ export default function Drinks() {
           <hr />
           <h2>Pina Colada</h2>
           <p>A creamy tropical sensation with coconut, pineapple, and rum.</p>
-          <h3>$6.50</h3>
+          <h3>$7.50</h3>
         </div>
 
         <div className={styles.item}>
           <hr />
           <h2>Strawberry Daiquiri </h2>
           <p>The perfect drink for a hot summer day.</p>
-          <h3>$6.50</h3>
+          <h3>$7.50</h3>
         </div>
 
         <div className={styles.item}>
@@ -171,7 +171,7 @@ export default function Drinks() {
             Imagine steel drums, sunsets, and sand as you sip on this tropical
             liberation made with rum, strawberry, coconut, and Mango.
           </p>
-          <h3>$6.50</h3>
+          <h3>$7.50</h3>
         </div>
 
         <div className={styles.item}>
